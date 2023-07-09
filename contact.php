33c228e3a3e820f82b6379704a0b9bbd72fa90f3
mrKesquivel17 <132179436+mrKesquivel17@users.noreply.github.com>
2023-07-09 11:25:32 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,21 +1,27 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message'])){
-    echo "All fields are required.";
-    exit();
-}
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-
-$to = "mrkesquivel17@gmail.com";
-$subject = "New Contact Form Submission";
-$body = "Name: $name\nEmail: $email\nMessage: $message";
-
-$headers = "From: $email";
-if(mail($to, $subject, $body, $headers)){
-    echo "Message sent successfully!";
-} else{
-    echo "Oops! An error occurred.";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validate form fields
+    if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message'])) {
+        echo "All fields are required.";
+        exit();
+    }
+    
+    // Sanitize form inputs
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    // Prepare email content
+    $to = "mrkesquivel17@gmail.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+    $headers = "From: $email";
+    
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Oops! An error occurred.";
+    }
 }
 ?>
